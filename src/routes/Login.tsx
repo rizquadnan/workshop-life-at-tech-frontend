656f633c759379fc1a44b315bbad0f120cf8d076
rizquadnan <madnanrizqullah@gmail.com>
2024-02-03 16:58:52 +0300

    done login page

--- src/routes/Login.tsx
+++ src/routes/Login.tsx
@@ -1,12 +1,32 @@
-import { useLocation } from "react-router-dom";
+import { Anchor, Button, Stack, Text, TextInput, Title } from "@mantine/core";
+import { Link, useSearchParams } from "react-router-dom";
 const Login = () => {
-  const location = useLocation();
+  const [searchParam] = useSearchParams();
 
   return (
-    <div>
-      <h1>Login</h1>
-      <pre>{JSON.stringify(location)}</pre>
-    </div>
+    <Stack>
+      <Title order={1}>Login</Title>
+      <Text>Hi {searchParam.get("user_type")}!</Text>
+      <Stack>
+        <TextInput label="Email" />
+        <TextInput label="Password" />
+        <Button>Submit</Button>
+        <Stack>
+          <Anchor
+            component={Link}
+            to={`/register?user_type=${searchParam.get("user_type")}`}
+          >
+            Register
+          </Anchor>
+          <Anchor
+            component={Link}
+            to={`/forgot-password?user_type=${searchParam.get("user_type")}`}
+          >
+            Forgot Password
+          </Anchor>
+        </Stack>
+      </Stack>
+    </Stack>
   );
 };
 
